70d2ef51c6b050bb1f8ea2e03f94a6d17440c427
T-A-S-H-U-N <ttashun.15@cse.mrt.ac.lk>
2020-03-05 06:35:37 +0530

    Merge branch '1'
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(2);
+        System.out.println(6);
+        System.out.println(7);
+
     }
     public void iniy(){}
 }
