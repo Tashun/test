8af7ca413e7d3ec0e4429b1f49f232ac8e2ff795
T-A-S-H-U-N <ttashun.15@cse.mrt.ac.lk>
2020-03-23 14:06:07 +0530

    Merge branch '1'
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.javaws.IconUtil;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(2);
@@ -6,7 +8,13 @@ public class Main {
         System.out.println(2);
         System.out.println(9);
         System.out.println(10);
+        System.out.println(15);
         System.out.println(45);
+        System.out.println();
+        System.out.println("21315");
+        System.out.println("tas");
+        System.out.println("master");
+        System.out.println("com");
         System.out.println();
 
     }
