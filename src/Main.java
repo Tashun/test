488c35dacf7de5e147f34f82ff6d96cf27cf310f
T-A-S-H-U-N <ttashun.15@cse.mrt.ac.lk>
2020-03-23 14:06:07 +0530

    Merge branch '1'
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,15 @@ public class Main {
         System.out.println(9);
         System.out.println(10);
         System.out.println(15);
+        System.out.println(15);
         System.out.println(16);
         System.out.println(17);
         System.out.println(45);
+        System.out.println();
+        System.out.println("21315");
+        System.out.println("tas");
+        System.out.println("master");
+        System.out.println("com");
         System.out.println();
         System.out.println("21315");
         System.out.println("tas");
